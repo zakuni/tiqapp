e32ce8aecc7426deb1aaf340ba0098770f77e3ef
zakuni <kunio038@gmail.com>
2012-06-12 20:21:39 +0900

    Initial Commit

---
+++ tiqapp/AppDelegate.rb
@@ -0,0 +1,15 @@
+#
+#  AppDelegate.rb
+#  tiqapp
+#
+#  Created by 邦雄 小沢 on 12/06/12.
+#  Copyright 2012年 __MyCompanyName__. All rights reserved.
+#
+
+class AppDelegate
+    attr_accessor :window
+    def applicationDidFinishLaunching(a_notification)
+        # Insert code here to initialize your application
+    end
+end
+
---
+++ tiqapp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  tiqapp
+//
+//  Created by 邦雄 小沢 on 12/06/12.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#import <MacRuby/MacRuby.h>
+
+int main(int argc, char *argv[])
+{
+    return macruby_main("rb_main.rb", argc, argv);
+}
